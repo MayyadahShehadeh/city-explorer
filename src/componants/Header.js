e73f2972de6331f74fc8f38dd29dd3242e288e45
mayyadah <miss.mayadah1@gmail.com>
2021-06-27 22:35:46 +0300

    add api

---
+++ src/componants/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+  class Header extends Component {
+    render() {
+        return (
+            <div>
+               <h1>City Explore App</h1> 
+            </div>
+        )
+    }
+}
+export default Header
