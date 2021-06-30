0b4b96f7c01d3bced43af7e62ea6edb513b2bf1a
mayyadah <miss.mayadah1@gmail.com>
2021-06-30 20:30:45 +0300

    lab08

--- src/componants/Main.js
+++ src/componants/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Form ,Button,Image } from 'react-bootstrap';
 import Weather from './Weather';
 import axios from 'axios';
+let iqKey=process.env.REACT_APP_LOCATIONIQ_API_KEY
 
  class Main extends Component {
 
@@ -9,11 +10,12 @@ import axios from 'axios';
         super(props);
         this.state={
           cityName:'',
-          lon:'',
-          lat:'',
+          longitude:'',
+          latitude:'',
           displayError: false,
           displayMap: true,
-          weatherData: []
+          weatherData: [],
+          // movieData=[]
 
         }
         console.log(this.state.weatherData);
@@ -23,21 +25,31 @@ import axios from 'axios';
       submitCity= async(e)=>{
         e.preventDefault()
         try{ 
-        let axiosResponse = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=pk.064267016d83e6a3caea3cf7190b14a8&city=${this.state.cityName}&format=json`)
-        const axiosLocalApi= await axios.get(`${process.env.REACT_APP_BACKEND_URL}/weather?lat=31.95&lon=35.91&searchQuery=${this.state.cityName}`)
-
-        this.setState({
-          cityName: axiosResponse.data[0].display_name,
-          lon:axiosResponse.data[0].lon,
-          lat:axiosResponse.data[0].lat,
-          displayError: false,
-          displayMap: true,
-          weatherData:axiosLocalApi.data
-      
           
-        });
-     
-      
+          let axiosResponse = await axios.get(`https://eu1.locationiq.com/v1/search.php?key=${iqKey}&city=${this.state.cityName}&format=json`)
+          
+          this.setState({
+            longitude:axiosResponse.data[0].lon,
+            latitude:axiosResponse.data[0].lat,
+            displayError: false,
+            displayMap: true,
+            
+            
+          });
+          
+          const axiosLocalApi= await axios.get(`http://localhost:8000/weather?lat=${this.state.latitude}&lon=${this.state.longitude}`)
+          
+          this.setState({
+            weatherData:axiosLocalApi.data,
+          });
+
+          // const moviApi= await axios.get(`${KeyLocal}/movies?city=${this.state.cityName}`)
+          // this.setState({
+          //   movieData:moviApi.data,
+          //   cityName: moviApi.data[0].display_name,
+          // });
+
+
         console.log(axiosLocalApi.data)
       }
         catch  (error) {
@@ -66,34 +78,39 @@ import axios from 'axios';
                    }
 
         
-        {/* ----------------------------------- add bootsrap form ----------------- */}
-        <Form onSubmit={this.submitCity}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Enter City Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter City Name" onChange={(e) => { this.cityChangeHandler(e) }} />
-          </Form.Group>
+            {/* ----------------------------------- add bootsrap form ----------------- */}
+            <Form onSubmit={this.submitCity}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Enter City Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter City Name" onChange={(e) => { this.cityChangeHandler(e) }} />
+              </Form.Group>
 
-          <Button variant="primary" type="submit">Explore!</Button>
-        </Form>
-          <p>{this.state.cityName}</p>
-          <p>{this.state.lon}</p>
-          <p>{this.state.lat}</p>
-          {/* --------------------------------------------------------------------------- */}
-         
-         {this.state.displayMap &&
-         <div>
+              <Button variant="primary" type="submit">Explore!</Button>
+            </Form>
+            <p>{this.state.cityName}</p>
+            <p>{this.state.longitude}</p>
+            <p>{this.state.latitude}</p>
+            {/* --------------------------------------------------------------------------- */}
 
-             <Image src={`https://maps.locationiq.com/v3/staticmap?key=pk.064267016d83e6a3caea3cf7190b14a8&center=${this.state.lat},${this.state.lon}&zoom=20&format=png`} rounded />        
-         </div>
-         }
+            {this.state.displayMap &&
+              <div>
+                <Image src={`https://maps.locationiq.com/v3/staticmap?key=${iqKey}&center=${this.state.latitude},${this.state.longitude}&zoom=10&format=png`} />
+              </div>
+            }
+            {/* ----------------------------------------------------------------------------------------------------------- */}
+            {this.state.weatherData.map(value => {
 
-            { this.state.weatherData.map(weatherData=>{
-              
-              return  <Weather desc={weatherData.description} date={weatherData.date}/>
-
+              return <Weather desc={value.description} date={value.date} />
             })
             }
-            </div>
+
+            {/* {
+              this.state.moviesData.map(value => {
+                return <Movies title={value.title} image_url={value.image_url}
+                vote_average={value.vote_average} />
+              })} */}
+
+          </div>
         )
     }
 }
